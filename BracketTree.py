d63c1a202268103e82134e4f2bdd35faab7a4301
Spike Chen <yankunc@uoregon.edu>
2023-05-24 04:51:55 -0700

    finish the winner function

--- BracketTree.py
+++ BracketTree.py
@@ -1,77 +1,91 @@
-class Node(object):
-    def __init__(self, name, layer):
-        self.layer = layer
-        self.parent = None
+class Node():
+    def __init__(self, data):
+        self.data = data
         self.left = None
+        self.parent = None
         self.right = None
-        self.name = name
-class Tree(object):
+        self.score = None
+
+class Tree():
     def __init__(self):
-        self.root = Node("Pending",1)
+        self.root = None
 
-    def createTree(self,current_node, teamnum):
-        if current_node.layer < teamnum:
-            current_node.left = Node("None", current_node.layer * 2)
-            current_node.right = Node("None", current_node.layer * 2)
-            current_node.left.parent = current_node
-            current_node.right.parent = current_node
-            self.createTree(current_node.left, teamnum)
-            self.createTree(current_node.right, teamnum)
+    def add(self, data):
 
-    def insertname(self, namelist):
-        for name in namelist:
-            current_node = self.root
-            while (current_node.left.left is not None):
-                if(current_node.left.name == "None"):
-                    current_node = current_node.left
-                elif(current_node.left.name == "Pending") and (current_node.right.name == "Pending"):
-                    current_node.name = "Pending"
-                    current_node = current_node.parent.right    
+        node = Node(data)
+        if self.root == None:
+            self.root = node
+        else:
+            myqueue = []
+            treeNode = self.root
+            myqueue.append(treeNode)
+            while myqueue:
+                treeNode = myqueue.pop(0)
+                if not treeNode.left:
+                    treeNode.left = node
+                    treeNode.left.parent = treeNode
+                    return
+                elif not treeNode.right:
+                    treeNode.right = node
+                    treeNode.right.parent = treeNode
+                    return
                 else:
-                    current_node = current_node.right
+                    myqueue.append(treeNode.left)
+                    myqueue.append(treeNode.right)
+    def BFS(self):
+        if self.root == None:
+            return
+        queue = []
+        queue.append(self.root)
 
-            if current_node.left.name == "None":
-                current_node.left.name = name
-            else:
-                current_node.right.name = name
-                current_node.name = "Pending"
-        self.check(self.root)
+        while queue:
+            now_node = queue.pop(0)
+            print(now_node.data)
+            if now_node.left != None:
+                queue.append(now_node.left)
+            if now_node.right != None:
+                queue.append(now_node.right)
 
-    def check(self, node):
-        if node.left is not None:
-            self.check(node.left)
-        if (node.left is not None) and (node.right is not None):
-            node.name = "Pending"
-        if node.right is not None:
-            self.check(node.right)
+    def setWinner(self, team, score):
+        if self.root == None:
+            return
+        queue = []
+        queue.append(self.root)
+        while queue:
+            now_node = queue.pop(0)
+            if now_node.data == team:
+                now_node.parent.data = team
+                now_node.parent.score = score
+                return
+            if now_node.left != None:
+                queue.append(now_node.left)
+            if now_node.right != None:
+                queue.append(now_node.right)
 
 
-
-                
-           
-            
-
-
-                    
-            
-                
-                
-
-    def print(self,node):
-        self.__print(node)
-        
-    def __print(self,current_node):
-        if current_node is not None:
-            self.__print(current_node.left)
-            print(current_node.name, end = ' ')
-            self.__print(current_node.right)
-def main():
-    t = Tree()
-    t.createTree(t.root,8)
-    t.print(t.root)
-    print()
-    t.insertname(["1","2", "3","4", "5","6","7","8"])
-    t.print(t.root)
+def insertTeam(teamlist):
+    tree = Tree()
+    l = len(teamlist)
+    for i in range(1, l * 2):
+        if i < l:
+            tree.add("TBD")
+        else:
+            tree.add(teamlist[l*2 - i-1])
     
+    return tree
 
-main()
+def test():
+    teamlist = ["1","2","3","4"]
+    t = insertTeam(teamlist)
+    t.BFS()
+    t.setWinner("1","3:0")
+    t.setWinner("4","2:0")
+    print()
+    t.BFS()
+    t.setWinner("1","3:0")
+    print()
+    t.BFS()
+
+test()
+
+    
