93d03a151b611b796775783a76b75962918bb0cc
JarenAlexander <74381469+JarenAlexander@users.noreply.github.com>
2023-05-30 00:20:28 -0700

    Update pythonFlaskV0.py

--- pythonFlaskV0.py
+++ pythonFlaskV0.py
@@ -13,16 +13,13 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/host')
 def host():
     return render_template('host.html')
 
 
-@app.route('/signup')
 def signup():
     return render_template('signup.html')
 
 
-@app.route('/tournament')
 def tournament():
     return render_template('tournament.html')
