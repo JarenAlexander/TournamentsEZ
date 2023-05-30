0fcdf4082c45b6db639d3872b87466a71d0fe165
JarenAlexander <74381469+JarenAlexander@users.noreply.github.com>
2023-05-30 00:03:07 -0700

    Update pythonFlaskV0.py

--- pythonFlaskV0.py
+++ pythonFlaskV0.py
@@ -1,8 +1,8 @@
 from flask import Flask,  render_template
 import sqlite3
 
-app = Flask(__name__, template_folder='public')
 app = Flask(__name__)
+app = Flask(__name__, template_folder='public')
 
 DATABASE = 'database.db'
 
