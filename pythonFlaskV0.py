465f04b6ee2daf8cace9bd6962b53af6cc993f40
JarenAlexander <74381469+JarenAlexander@users.noreply.github.com>
2023-05-30 00:15:44 -0700

    Update pythonFlaskV0.py

--- pythonFlaskV0.py
+++ pythonFlaskV0.py
@@ -9,6 +9,20 @@ DATABASE = 'database.db'
 
 
 @app.route('/')
-
 def index():
     return render_template('index.html')
+
+
+@app.route('host')
+def host():
+    return render_template('host.html')
+
+
+@app.route('signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('tournament')
+def tournament():
+    return render_template('tournament.html')
