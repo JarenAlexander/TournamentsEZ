fc1f669d4ffa6184c791248bef858d6317d088c0
brettadrianfox <104589484+brettadrianfox@users.noreply.github.com>
2023-06-07 20:53:04 -0700

    Delete database_sqlite directory

--- database_sqlite/proj2_query2.sql
+++
@@ -1,6 +0,0 @@
--- SQLite
-SELECT Tournament.name
-FROM Tournament
-    JOIN PlayerTournament ON (Tournament.name=PlayerTournament.tournament_name)
-    JOIN Player ON (PlayerTournament.player_email=Player.email)
-WHERE Player.email = "player9@example.com";
--- database_sqlite/proj2_query3.sql
+++
@@ -1,4 +0,0 @@
--- SQLite
-SELECT Tournament.name
-FROM Tournament
-WHERE Tournament.date = "2023-10-20";
