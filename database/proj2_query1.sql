6a6b34d8a9b952b224ed005c879bce855a79ad73
brettadrianfox <104589484+brettadrianfox@users.noreply.github.com>
2023-06-09 19:21:54 -0700

    Update proj2_query1.sql

--- database/proj2_query1.sql
+++ database/proj2_query1.sql
@@ -1,4 +1,15 @@
--- SQLite
+/*
+
+This is a sample query, used for testing, that checks which players are participating in the tournament Super Smash Showdown.
+
+Authors: Jaren Alexander, Yankun Chen, Brett Fox, Kevin Truong, Sam Windheim
+
+Group Name: TournamentsEZ
+
+Last Modification Date: 6/9/2023
+
+*/
+
 -- Checking which users are participating in Super Smash Showdown
 SELECT Player.fname, Player.lname, Player.email
 FROM Player 
